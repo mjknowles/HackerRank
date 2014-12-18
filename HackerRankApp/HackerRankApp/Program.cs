e009f93c990f454d83a0995d05d5b38da945aa90
mjknowles23 <mjknowles23@gmail.com>
2014-12-17 23:53:21 -0600

    Implementd four new answers. working on XOR problem.

--- HackerRankApp/HackerRankApp/Program.cs
+++ HackerRankApp/HackerRankApp/Program.cs
@@ -10,6 +10,65 @@ namespace HackerRankApp
     {
         static void Main(string[] args)
         {
+            int res;
+            int _l;
+            _l = Convert.ToInt32(Console.ReadLine());
+
+            int _r;
+            _r = Convert.ToInt32(Console.ReadLine());
+
+            res = maxXor(_l, _r);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+
+        private static int maxXor(int _l, int _r)
+        {
+            List<int[]> byteReps = GetIntBitArray(_l, _r);
+            int xorVal = 0;
+            int maxVal = 0;
+            for (int j = 0; j < _r - _l; j++)
+            {
+                for (int k = j; k < _r - _l; k++)
+                {
+                    xorVal = 0;
+                    for (int i = 0; i < 10; i++)
+                    {
+                        if (byteReps[j][i] != byteReps[k][i])
+                        {
+                            xorVal += Convert.ToInt32(Math.Pow(2, i));
+                        }
+                    }
+                    if (xorVal > maxVal)
+                        maxVal = xorVal;
+                }
+            }
+            return maxVal;
+        }
+
+        private static List<int[]> GetIntBitArray(int in1, int in2)
+        {
+            List<int[]> bitArrayList = new List<int[]>();
+            for (int j = in1; j <= in2; j++)
+            {
+                int[] numByteRep = new int[10];
+                int tempNum = j;
+                int numToSecondPower;
+
+                for (int i = numByteRep.Length - 1; i > -1; i--)
+                {
+                    numToSecondPower = Convert.ToInt32(Math.Pow(2, i));
+                    if (j >= numToSecondPower)
+                    {
+                        numByteRep[i] = 1;
+                        tempNum = tempNum - numToSecondPower;
+                    }
+                    else
+                        numByteRep[i] = 0;
+                }
+                bitArrayList.Add(numByteRep);
+            }
+            return bitArrayList;
         }
     }
 }
--- HackerRankApp/HackerRankApp/Warmup.cs
+++ HackerRankApp/HackerRankApp/Warmup.cs
@@ -8,6 +8,35 @@ namespace ConsoleApplication1
 {
     class Warmup
     {
+        public void UtopianTree()
+        {
+            int totalInputs = Convert.ToInt32(Console.ReadLine());
+            int[] allNumberInputs = new int[totalInputs];
+            int maxNum = 0;
+            for (int i = 0; i < totalInputs; i++)
+            {
+                int userNum = Convert.ToInt32(Console.ReadLine());
+                allNumberInputs[i] = userNum;
+                if (userNum > maxNum)
+                    maxNum = userNum;
+            }
+
+            long[] heightByCycleArray = new long[maxNum + 1];
+            heightByCycleArray[0] = 1;
+            for (int i = 1; i < maxNum + 1; i++)
+            {
+                if (i % 2 == 1)
+                    heightByCycleArray[i] = 2 * heightByCycleArray[i - 1];
+                else
+                    heightByCycleArray[i] = 1 + heightByCycleArray[i - 1];
+            }
+
+            for (int i = 0; i < totalInputs; i++)
+            {
+                Console.WriteLine(heightByCycleArray[allNumberInputs[i]]);
+            }
+        }
+
         public void isFibo()
         {
             int totalInputs = Convert.ToInt32(Console.ReadLine());
